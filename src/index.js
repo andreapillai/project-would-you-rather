30c12d718b726539839173a0d2c78e355494518f
andreapillai <andrea.pillai@gmail.com>
2020-07-24 19:33:16 +0200

    Create store, connect to toolkit, add Provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import reducer from "./reducers";
+
+const store = configureStore({
+  reducer,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
