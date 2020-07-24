12e6cdb84ae1c5ad941d5f2b99c4ea9190c6c120
andreapillai <andrea.pillai@gmail.com>
2020-07-25 00:13:01 +0200

    Render Users and Questions as Collections

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "./../actions/shared";
+import { Row, Col, Collection, CollectionItem } from "react-materialize";
 import NavBar from "./NavBar";
 
 export class App extends Component {
@@ -9,13 +10,42 @@ export class App extends Component {
   }
 
   render() {
+    const { authUser, userIds, questionIds } = this.props;
     return (
       <div>
         <NavBar />
-        <h1>Would You Rather... ?</h1>
+        {authUser && (
+          <div className="container">
+            <h1 className="center">{authUser}</h1>
+            <Row>
+              <Col m={6} s={12}>
+                <Collection header="Users">
+                  {userIds.map((id) => (
+                    <CollectionItem key={id}>{id}</CollectionItem>
+                  ))}
+                </Collection>
+              </Col>
+              <Col m={6} s={12}>
+                <Collection header="Questions">
+                  {questionIds.map((id) => (
+                    <CollectionItem key={id}>{id}</CollectionItem>
+                  ))}
+                </Collection>
+              </Col>
+            </Row>
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authUser, users, questions }) {
+  return {
+    authUser,
+    userIds: Object.keys(users),
+    questionIds: Object.keys(questions),
+  };
+}
+
+export default connect(mapStateToProps)(App);
